bdb9da1757fa37f9dc5722992436af6f2bfdb0af
Rajasimha Reddy <rajasimha5599@gmail.com>
2021-03-15 18:58:52 +0530

    create block and blockchain classes along with hash calculation and mining

---
+++ main.js
@@ -0,0 +1,82 @@
+const SHA256 = require('crypto-js/sha256');
+
+class Block {
+
+    // index is the sequence number of this block in the chain
+    // timestamp is when this was created
+    // data to be stored in the block
+    // previousHash ensures integrity of blockchain
+    constructor(index, timestamp, data, previousHash = '') {
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = this.calculateHash();
+        this.nonce = 0;
+    }
+
+    calculateHash() {
+        return SHA256(this.index + this.previousHash + this.nonce + this.timestamp + JSON.stringify(this.data)).toString();
+    }
+
+    mineBlock(difficulty) {
+        while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+        console.log("Block mined", this.hash)
+    }
+}
+
+
+class Blockchain {
+    constructor() {
+        this.chain = [this.createGenesisBlock()];
+        this.difficulty = 5;
+    }
+
+    createGenesisBlock() {
+        return new Block(0, new Date(), "Genesis block", "0")
+    }
+
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1]
+    }
+
+    addBlock(newBlock) {
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.mineBlock(this.difficulty);
+        this.chain.push(newBlock);
+    }
+
+    isChainValid() {
+        for (let i = 1; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+
+            if (currentBlock.hash !== currentBlock.calculateHash()) {
+                return false;
+            }
+
+            if (currentBlock.previousHash !== previousBlock.hash) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+let kingCoin = new Blockchain();
+
+console.log('Mining block 1');
+kingCoin.addBlock(new Block(1, new Date(), {
+    amount: 10
+}))
+console.log('Mining block 2');
+
+kingCoin.addBlock(new Block(2, new Date(), {
+    amount: 50
+}))
+
+
+// console.log(JSON.stringify(kingCoin, null, 4))
